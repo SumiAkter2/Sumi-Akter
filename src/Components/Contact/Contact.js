7e6424b55014e57bd8d27f8a3bcc67f95a31a793
SumiAkter2 <akteralisha36@gmail.com>
2023-01-13 22:33:15 +0600

    added button

--- src/Components/Contact/Contact.js
+++ src/Components/Contact/Contact.js
@@ -2,15 +2,16 @@ import React from "react";
 import { IoMdMail } from "react-icons/io";
 import Button from "../Button/Button";
 import { ImLocation } from "react-icons/im";
+import { IoMdSend } from "react-icons/io";
 import { BsFillTelephoneForwardFill } from "react-icons/bs";
 import "./Contact.css";
 
 const Contact = () => {
   return (
     <div className="w-3/4">
-      <h1 className="text-center mb-12 text-secondary">Contact Me</h1>
-      <div className="py-12 lg:flex  items-center">
-        <div class="card font-bold font-sans p-12 mx-6">
+      <h1 className="text-center text-secondary">Contact Me</h1>
+      <div className="py-12 lg:flex  items-center ">
+        <div class="card font-bold font-sans p-12 mx-6 mt-12">
           <h3 className=" text-secondary text-3xl">Contact Information</h3>
           <div>
             <p className="flex  justify-start items-center mt-6">
@@ -22,14 +23,15 @@ const Contact = () => {
             </p>
             <p>sumiakter6335@gmail.com</p>
             <p className="flex  justify-start items-center mt-6">
-              <BsFillTelephoneForwardFill className="mr-4" /> Address
+              <BsFillTelephoneForwardFill className="mr-4" />
+              Phone Number
             </p>
             <p>+8801718723057</p>
           </div>
         </div>
 
         {/* ======================= */}
-        <div className="card  text-center font-bold font-sans p-12 mx-6">
+        <div className="card  text-center font-bold font-sans p-12 mx-6 mt-12">
           <h3 className=" text-secondary text-3xl">Please Contact Me</h3>
           <form action="">
             <div className="flex">
@@ -44,7 +46,7 @@ const Contact = () => {
                 <i></i>
               </div>
             </div>
-            <div className="flex">
+            <div className="flex ">
               <div class="inputbox ml-4 mb-2">
                 <input required="required" type="number" />
                 <span>Phone </span>
@@ -62,7 +64,11 @@ const Contact = () => {
               <i></i>
             </div>
             <buttons className="mt-2 ">
-              <span class="box">Hover!</span> 
+              <span class="box ">
+                <p className="flex justify-center items-center">
+                  <IoMdSend className="mr-4" /> Send
+                </p>
+              </span>
             </buttons>
           </form>
         </div>
